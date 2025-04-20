82b79543fc00aea3adee00cf9f8fdbcece989d54
maria <maria.manzano@students.salle.url.edu>
2025-04-20 20:35:13 +0200

    Generator, CoffeeMachine, Barista and Cafe classes added

---
+++ src/MARIA/Cafe.java
@@ -0,0 +1,8 @@
+package MARIA;
+
+public class Cafe extends Generator {
+
+    public Cafe(String name, float baseCost, double production, double timeInterval, String imagePath, double costIncrement) {
+        super(name, baseCost, production, timeInterval, imagePath, costIncrement);
+    }
+}
