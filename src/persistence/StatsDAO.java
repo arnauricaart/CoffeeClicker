774d86557e097c1a762401355415e9bd66d9ace5
HectorMillasSuarez <hmillas05@gmail.com>
2025-05-24 17:58:58 +0200

    Añadido algunos JavaDocs

--- src/persistence/StatsDAO.java
+++ src/persistence/StatsDAO.java
@@ -13,6 +13,8 @@ public interface StatsDAO {
      * This method will implement a way to get a List with all stats from one Game, searching by the Game Id.
      * @param gameID Number with the Game Id used in the search.
      * @return List of numbers, those numbers are the coffes of the game at every minute.
+     * @throws FileNotFound if the data source file is not found
+     * @throws DBGeneralException if a general database error occurs
      */
     public List<Integer> getStatsByGameId(int gameID) throws FileNotFound, DBGeneralException;
 
@@ -21,6 +23,7 @@ public interface StatsDAO {
      * @param gameId Number with the Game Id of the game of which the stats come from.
      * @param cafes Number of coffees of the game at the instant that the new register is made.
      * @param min Number with the minute of the game.
+     * @throws DBGeneralException if a general database error occurs
      */
     public void updateStats(int gameId, int cafes, int min) throws DBGeneralException;
 
--- src/persistence/UserDAO.java
+++ src/persistence/UserDAO.java
@@ -12,6 +12,7 @@ public interface UserDAO {
      * This method will implement a way to remove an user and all of its information.
      * @param email String with the User Mail.
      * @return Returns a boolean that will tell if everything went okey.
+     * @throws DBGeneralException if a general database error occurs
      */
     public boolean removeUserAndData(String email) throws DBGeneralException;
 
@@ -21,6 +22,8 @@ public interface UserDAO {
      * @param email String with the User Mail.
      * @param password String with the User Password.
      * @return Returns a boolean that will tell if everything went okey.
+     * @throws FileNotFound if the data source file is not found
+     * @throws DBGeneralException if a general database error occurs
      */
     public boolean registerUser(String username, String email, String password) throws FileNotFound, DBGeneralException;
 
@@ -28,6 +31,8 @@ public interface UserDAO {
      * This method will implment a way to check if a user exists searching it by the User Name.
      * @param username String with the User Name that will be used in the search.
      * @return Returns a boolean that will tell if it exists or not.
+     * @throws FileNotFound if the data source file is not found
+     * @throws DBGeneralException if a general database error occurs
      */
     public boolean checkUserExists(String username) throws FileNotFound, DBGeneralException;
 
@@ -36,6 +41,8 @@ public interface UserDAO {
      * @param userOrEmail String with the User Name or User Mail.
      * @param password String with the User Password.
      * @return Returns a String with the User Mail.
+     * @throws FileNotFound if the data source file is not found
+     * @throws DBGeneralException if a general database error occurs
      */
     public String getCorreoFromLogin(String userOrEmail, String password) throws FileNotFound, DBGeneralException;
 
@@ -43,6 +50,8 @@ public interface UserDAO {
      * This method will implement a way to check if a User Mail exists.
      * @param email String with the User Mail.
      * @return Returns a Boolean that will tell if the User Mail exists or not.
+     * @throws FileNotFound if the data source file is not found
+     * @throws DBGeneralException if a general database error occurs
      */
     boolean checkEmailExists(String email) throws FileNotFound, DBGeneralException;
 }
