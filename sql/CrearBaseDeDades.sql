44a74d2ffb7e22c7e82cd7e1ecde070fa7648443
gerim <gerimasblanch@gmail.com>
2025-05-25 20:02:48 +0200

    dumpfinal

--- sql/CrearBaseDeDades.sql
+++
@@ -1,5 +0,0 @@
-mysql -u root -p
-drop database cofeeclicker_schema;
-create database cofeeclicker_schema;
-use cofeeclicker_schema
-source C:/DPO_P2_CoffeeClicker/sql/Dump20250514ultimaversion.sql
--- sql/fixPartidaConstraint.sql
+++
@@ -1,8 +0,0 @@
-alter table cofeeclicker_schema.partida
-drop constraint partida_nombre_uk;
-
-
-alter table cofeeclicker_schema.partida
-    add constraint partida_nombre_uk
-        unique (Correo, Nombre);
-
